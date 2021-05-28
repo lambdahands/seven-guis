e9930faacd2a56ff2168fc0cc098dd6de2bce7ec
Pax <hipax@pm.me>
2021-05-28 17:18:30 -0400

    feat: Add Timer task, core.async dependency

--- src/main/seven_guis/core.cljs
+++ src/main/seven_guis/core.cljs
@@ -2,13 +2,15 @@
   (:require [seven-guis.tasks.counter :refer [counter]]
             [seven-guis.tasks.temperature-converter :refer [temperature-converter]]
             [seven-guis.tasks.flight-booker :refer [flight-booker]]
+            [seven-guis.tasks.timer :refer [timer]]
             [reagent.dom :as rdom]))
 
 (defn main []
   [:div
    [counter]
    [temperature-converter]
-   [flight-booker]])
+   [flight-booker]
+   [timer]])
 
 (defn init []
   (let [app-element (js/document.getElementById "app")]
